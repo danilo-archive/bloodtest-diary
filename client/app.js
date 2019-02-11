d2b2b18a2184d612486626dbc57f345a08449ed1
JacopoMadaluni <jacopo.madaluni@gmail.com>
2019-02-11 18:55:47 +0000

    Add templates endpoints in client/app.js

--- client/app.js
+++ client/app.js
@@ -8,14 +8,18 @@
 
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const ioClient = require('socket.io-client');
+//const server = require('http').createServer(app);
+//const io = require('socket.io')(server); // TODO remove not needed
+const ioClient = require('socket.io-client'); // TODO remove not needed
 const CONFIG_FILE_PATH = __dirname + '/config/app_config.json';
 const jsonController = require('./lib/json-controller');
 const bodyParser = require('body-parser');
 
 
+module.exports = {
+    init, getAllPatients, getAllTests, getTestsOfPatient, getTestsOnDate,
+    getOverdueTests, getTestsInWeek,
+}
 
 //start the app
 init(jsonController.getJSON(CONFIG_FILE_PATH))
@@ -25,14 +29,17 @@ init(jsonController.getJSON(CONFIG_FILE_PATH))
  * the configuration settings from a specific file.
  * @param {json} conf The JSON config file
  */
+ // TODO
+ // completely refactor this, init should initialize the socket with the server
+ // should not provide get/post endpoints
 function init(conf)
 {
     let port = conf.port;
     let staticFolder = conf.staticFolder;
     var indexFile = conf.indexFile;
     var server_port = conf.server_port;
-    const client = ioClient.connect("http://localhost:"+server_port);
-    server.listen(port);
+    const socket = ioClient.connect("http://localhost:"+server_port);
+    //server.listen(port);
     /*
      * Express connection.
      */
@@ -47,11 +54,11 @@ function init(conf)
     //On post, try to login user; for now
     app.post('/',function (req,res)
     {
-        client.emit('log', {username:req.body.username, password: req.body.password},function()
+        socket.emit('log', {username:req.body.username, password: req.body.password},function()
         {
 
         });
-        client.on('auth', function(user)
+        socket.on('auth', function(user)
         {
           if(!res.headersSent)
           {
@@ -70,5 +77,87 @@ function init(conf)
             }
           }
         });
+    });
+}
+
+/**
+ * Function to be called when all patients have to be retrieved.
+ * @param callback The callback function to be called on response
+ * TODO eventually change name of the function.
+ * TODO eventually change name of the callback.
+ */
+function getAllPatients(callback){
+    socket.emit('getAllPatients');
+    socket.on("getAllPatientsResponse", res => {
+        callback(res);
+    });
+}
+
+/**
+ * Function to be called when all tests have to be retrieved.
+ * @param callback The callback function to be called on response
+ * TODO eventually change name of the function.
+ * TODO eventually change name of the callback.
+ */
+function getAllTests(callback){
+    socket.emit('getAllTests');
+    socket.on('getAllTestsResponse', res => {
+        callback(res);
+    });
+}
+
+/**
+ * Function to be called when all tests of a patient are needed.
+ * @param patientId The id of the patient we want to retrieve the information of.
+ * @param callback The callback function to be called on response
+ * TODO eventually change name of the function.
+ * TODO eventually change name of the callback.
+ */
+function getTestsOfPatient(patientId, callback){
+    socket.emit('getTestsOfPatient', patientId);
+    socket.on('getTestsOfPatientResponse', res => {
+        callback(res);
+    });
+}
+
+/**
+ * Function to be called when all tests on a date are needed.
+ * @param date The id of the patient we want to retrieve the information of.
+ * @param callback The callback function to be called on response
+ * TODO eventually change name of the function.
+ * TODO eventually change name of the callback.
+ */
+function getTestsOnDate(date, callback){
+    socket.emit('getAllTestsOnDate', date);
+    socket.on('getAllTestsOnDateResponse', res => {
+        callback(res);
+    });
+}
+
+/**
+ * Function to be called when all overdue tests are needed.
+ * @param callback The callback function to be called on response
+ * TODO eventually change name of the function.
+ * TODO eventually change name of the callback.
+ */
+function getOverdueTests(callback){
+    socket.emit('getOverdueTests');
+    socket.on('getOverdueTestsResponse', res => {
+        callback(res);
+    });
+}
+
+/**
+ * Function to be called when all tests in a week are needed.
+ * @param date Any date on the week we want to select
+ * @param anydayTestsOnly True if you only want the tests that are not scheduled on a particular day.
+ * @param callback The callback function to be called on response
+ * TODO eventually change name of the function.
+ * TODO eventually change name of the callback.
+ */
+function getTestsInWeek(date, anydayTestsOnly=false, callback){
+    socket.emit('getTestsInWeek', date, anydayTestsOnly);
+    socket.on('getTestsInWeekResponse', res => {
+        callback(res);
     });
 }
