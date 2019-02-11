a5796a21d27ca95b940d8114405f28467e18df1c
mateusznowak98 <mateusz.michal.nowak98@gmail.com>
2019-02-11 15:29:06 +0000

    Add basic login functionality

--- client/app.js
+++ client/app.js
@@ -1,17 +1,21 @@
 /**
- * The logic that works as back-end for the app's UI. 
+ * The logic that works as back-end for the app's UI.
  * It communicates with an external server, queries and sends update requests.
  * @module app
- * @author Danilo Del Busso
- * @version 0.0.1
+ * @author Danilo Del Busso, Mateusz Nowak
+ * @version 0.0.2
  */
 
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const ioClient = require('socket.io-client');
 const CONFIG_FILE_PATH = __dirname + '/config/app_config.json';
 const jsonController = require('./lib/json-controller');
+const bodyParser = require('body-parser');
+
+
 
 //start the app
 init(jsonController.getJSON(CONFIG_FILE_PATH))
@@ -21,20 +25,50 @@ init(jsonController.getJSON(CONFIG_FILE_PATH))
  * the configuration settings from a specific file.
  * @param {json} conf The JSON config file
  */
-function init(conf) {
-
+function init(conf)
+{
     let port = conf.port;
     let staticFolder = conf.staticFolder;
-    let indexFile = conf.indexFile;
-
+    var indexFile = conf.indexFile;
+    var server_port = conf.server_port;
+    const client = ioClient.connect("http://localhost:"+server_port);
+    server.listen(port);
     /*
-     * Express connection. 
+     * Express connection.
      */
     app.use(express.static(__dirname + staticFolder));
-    app.get('/', function (req, res, next) {
-        res.sendFile(indexFile);
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.get('/', function (req,res)
+    {
+        res.sendFile(__dirname +staticFolder+"/" +indexFile);
     });
 
-    server.listen(port);
+    //On post, try to login user; for now
+    app.post('/',function (req,res)
+    {
+        client.emit('log', {username:req.body.username, password: req.body.password},function()
+        {
 
+        });
+        client.on('auth', function(user)
+        {
+          if(!res.headersSent)
+          {
+            console.log("Logging in here...")
+            if(user)
+            {
+                console.log("Logged");
+                //Random redirect for now
+                res.redirect('https:google.com');
+            }
+            else
+            {
+                console.log("Unsuccesful login attempt");
+                //Redirect back to login
+                res.sendFile(__dirname +staticFolder+"/" +indexFile);
+            }
+          }
+        });
+    });
 }
