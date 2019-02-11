d31e708805194553642174e3b7919cb12a130953
JacopoMadaluni <jacopo.madaluni@gmail.com>
2019-02-11 18:25:27 +0000

    add basic endpoints to server/app.js

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
  * The module responsible for all the queries on the dataabase
  * and processing of the data retrived.
  * @module server
- * @author Mateusz Nowak
+ * @author Mateusz Nowak & Jacopo Madaluni
  * @version 0.0.1
  */
 
@@ -15,11 +15,48 @@ var authenticator = require("./lib/authenticator.js");
 http.listen(3265);
 io.on('connection',function(socket)
 {
-    socket.on('log', function(user)
-    {
+    socket.on('log', (user) => {
         console.log("Authorizing here...")
         //Function to get users here
         //Return the users in emit on the bottom
         socket.emit('auth', authenticator.canLogin(user));
     });
+
+    socket.on('getAllPatients', () => {
+        // TODO
+        // retrive all patients and return them as json
+    });
+
+    socket.on('getAllTests', () => {
+        // TODO
+        // retrieve all tests scheduled and return them as json
+    });
+
+    socket.on('getTestsOfPatient', (patientId) => {
+        // TODO
+        // given the id of the patient, return all the tests scheduled
+        // for that patient as json
+    });
+
+    socket.on('getTestsOnDate', (date) => {
+        // TODO
+        // given a date, return all tests on that date as json
+    });
+
+    socket.on('getTestsInWeek', (date, anydayTestsOnly=false) => {
+        // TODO
+        // given a date, retrieve the tests that are scheduled on that
+        // date week, and return them as json
+        // if anydayTestsOnly is true, return only the tests that don't have
+        // a particular day assigned
+    });
+
+
+    socket.on('getOverdueTests', () => {
+        // TODO
+        // retrieve all overdue tests and return them as json
+    });
+
+    // updates of database --------------------------------
+    // TODO add endpoints for diary updates
 });
