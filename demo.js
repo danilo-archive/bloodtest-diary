752bf2f82f25f5f7b4f3cdf4dfbd1fdee82974c0
k1764125 <luka.kralj@kcl.ac.uk>
2019-02-12 17:26:03 +0000

    Provide progress information in demo.js

--- demo.js
+++ demo.js
@@ -4,17 +4,42 @@ var date = new Date();
 
 var all = [];
 
-for (var i = 0; i < 1000000; i++) {
+for (var i = 1; i < 1000001; i++) {
     all.push(id_gen.generateUniqueID(date));
+    if (i % 10000 === 0) {
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write((i / 10000) + " %");
+    }
+}
+console.log("\nall generated");
+ 
+console.log("sorting");
+
+var sorted = all.sort();
+
+console.log("sorted");
+
+console.log("comparing:");
+
+var allUnique = true;
+
+for (var i = 0; i < sorted.length - 1; i++) {
+    if (i % (sorted.length/100) === 0) {
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write((i / sorted.length * 100) + " %");
+    }
+    
+    if (sorted[i] === sorted[i+1]) {
+        allUnique = false;
+        break;
+    }
 }
 
-function onlyUnique(value, index, self) { 
-    return self.indexOf(value) === index;
-}
-
-if (all.length === all.filter(onlyUnique).length) {
-    console.log("All IDs are unique.");
+if (allUnique) {
+    console.log("\nAll IDs are unique.");
 }
 else {
-    console.log("Duplicates were found!!");
+    console.log("\nDuplicates were found!!");
 }
