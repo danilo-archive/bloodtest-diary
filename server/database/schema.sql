17d7d7577b97e20c2a37a44f5908fbfd6a84c704
k1764125 <luka.kralj@kcl.ac.uk>
2019-02-13 01:15:19 +0000

    Update DB schema

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -14,9 +14,10 @@ DROP TABLE IF EXISTS Carer;
 DROP TABLE IF EXISTS Test;
 DROP TABLE IF EXISTS Patient;
 DROP TABLE IF EXISTS Laboratory;
+DROP TABLE IF EXISTS TokenControl;
 
 CREATE TABLE Laboratory (
-    lab_id INTEGER,
+    lab_id INTEGER AUTO_INCREMENT,
     lab_name VARCHAR(255),
     lab_email VARCHAR(255),
     lab_phone VARCHAR(10),
@@ -27,32 +28,44 @@ CREATE TABLE Patient (
     patient_no VARCHAR(10),
     patient_name VARCHAR(255) NOT NULL,
     patient_surname VARCHAR(255) NOT NULL,
-    patient_email VARCHAR(255) DEFAULT NULL,
-    patient_phone VARCHAR(10) DEFAULT NULL,
-    prefered_lab INTEGER DEFAULT NULL,
+    patient_email VARCHAR(255),
+    patient_phone VARCHAR(10),
+    prefered_lab INTEGER,
     PRIMARY KEY (patient_no),
     FOREIGN KEY (prefered_lab) REFERENCES Laboratory(lab_id)
 );
 
 CREATE TABLE Carer (
+    carer_id INTEGER AUTO_INCREMENT,
     patient_no VARCHAR(10),
-    carer_name VARCHAR(255) NOT NULL,
-    carer_email VARCHAR(255) DEFAULT NULL,
-    carer_phone VARCHAR(10) DEFAULT NULL,
-    relationship VARCHAR(100) DEFAULT "unknown",
-    PRIMARY KEY (patient_no, carer_name),
-    FOREIGN KEY (patient_no) REFERENCES Patient(patient_no)
+    carer_name VARCHAR(255),
+    carer_email VARCHAR(255),
+    carer_phone VARCHAR(10),
+    relationship VARCHAR(100),
+    PRIMARY KEY (carer_id),
+    FOREIGN KEY (patient_no) REFERENCES Patient(patient_no),
+    INDEX patient_no (patient_no)
 );
 
 CREATE TABLE Test (
     test_id INTEGER AUTO_INCREMENT,
     patient_no VARCHAR(10) NOT NULL,
+    added DATE NOT NULL,
     first_due_date DATE NOT NULL,
-    frequency VARCHAR(100) not NULL,
-    laboratory INTEGER DEFAULT NULL,
-    completed ENUM("yes", "no", "in review") NOT NULL DEFAULT "in review",
-    notes TEXT DEFAULT NULL,
+    frequency VARCHAR(100) NOT NULL,
+    laboratory INTEGER,
+    completed_status ENUM("yes", "no", "in review") NOT NULL DEFAULT "no",
+    completed_date DATE,
+    notes TEXT,
     PRIMARY KEY (test_id),
     FOREIGN KEY (patient_no) REFERENCES Patient(patient_no),
     FOREIGN KEY (laboratory) REFERENCES Laboratory(lab_id)
+);
+
+CREATE TABLE TokenControl (
+    token VARCHAR(50),
+    table_name ENUM("Carer", "Laboratory", "Patient", "Test") NOT NULL,
+    table_key VARCHAR(50) NOT NULL,
+    expiration DATETIME NOT NULL,
+    PRIMARY KEY (token)
 );
