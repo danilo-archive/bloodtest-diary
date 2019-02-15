7a35090f77317216bdcfe2331990a05a1ba2f718
Danilo Del Busso <danilo.delbusso1@gmail.com>
2019-02-15 14:29:01 +0000

    Add calendar controller to get dates with predetermined frequency format

---
+++ client/lib/calendar_controller.js
@@ -0,0 +1,73 @@
+
+/**
+ * Get all the next dates based on the frequency notation.
+ * It does NOT return the original date
+ * @param {string} frequency the frequency expressed as frequency notation
+ * @param {Date} startingDate the starting date (not included in the result)
+ * @returns {array<Date>} an array containing the next dates but not the given starting date
+ */
+function getNextDates(frequency, startingDate) {
+    const f = frequency.split(':');
+    if (f.length != 2 || f[1] < 0) {
+        console.error("The frequency format is wrong.")
+        return null;
+    }
+    const repetitions = parseInt(f[1]);    
+    const allDates = [];
+    let date = getNextDate(f[0], startingDate);
+  
+    for (let i = 0; i < repetitions; i++) {        
+        allDates.push(date);      
+        date = getNextDate(f[0], date);
+    }
+
+    return allDates;
+}
+
+/**
+ * Get the next date
+ * @param {string} frequency the frequency in the given format
+ * @param {date} startingDate the starting date from which to calculate the next date
+ */
+function getNextDate(frequency, startingDate) {
+    if (frequency === null || frequency.split('-').length > 2 || frequency.split('-').length < 1) {
+        console.error("Error in formatting date. Date is either null or not in the right format");
+        return null;
+    }
+    const f_value = parseInt(frequency.split('-')[0]); // the '3' in '3-Y'
+    const f_format = frequency.split('-')[1];// the 'Y' in '2-Y'
+    let date = null;
+    let year = startingDate.getFullYear();
+    const month = startingDate.getMonth();
+    let day = startingDate.getDate()
+
+    switch (f_format) {
+        case 'Y': {
+            year += f_value;
+            date = new Date(year, startingDate.getMonth(), startingDate.getDate());  //month is month_number-1 e.g. Jan is 0 and December is 11
+            break;
+        }
+        case 'W': {
+            day += (f_value * 7);
+            date = new Date(year, month, day);
+            break;
+        }
+        case 'D': {
+            day += f_value;
+            date = new Date(startingDate.getFullYear(), startingDate.getMonth(), day);
+            break;
+        }
+        default:
+            console.error("Error in formatting date");
+            return null;
+    }
+
+    if (validDate(date))
+        return date;
+
+
+    return null;
+}
+module.exports = {
+    getNextDates
+}
