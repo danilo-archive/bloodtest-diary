0629319355f4afc18e48373fcaf8bedbc97afe46
k1764125 <luka.kralj@kcl.ac.uk>
2019-02-17 17:20:40 +0000

    Implement control functions for delete and update queries

--- server/db_controller.js
+++ server/db_controller.js
@@ -1,9 +1,15 @@
 const mysql = require("mysql");
 
-// https://codeburst.io/node-js-mysql-and-promises-4c3be599909b
+/**
+ * Promisify mysql.
+ * Source: https://codeburst.io/node-js-mysql-and-promises-4c3be599909b
+ *
+ * @class Database
+ */
 class Database {
     constructor(config) {
         this.connection = mysql.createConnection(config);
+        this.connection.beginTransaction();
     }
     query(sql, args) {
         return new Promise((resolve, reject) => {
@@ -15,6 +21,7 @@ class Database {
         });
     }
     close() {
+        this.connection.commit();
         return new Promise((resolve, reject) => {
             this.connection.end((err) => {
                 if (err)
@@ -25,35 +32,50 @@ class Database {
     }
 }
 
-let databaseConfig = {
+const databaseConfig = {
     host: "localhost",
     user: "bloodTestAdmin",
     password: "Blood_admin1",
     database: "BloodTestDB"
 };
 
-function startsWith(sql, start) {
-    return sql.trim().toLowerCase().startsWith(start.toLowerCase());
+/**
+ * Shorthand function for comparing the start of a string. Leading spaces
+ * and capitalisation are ignored.
+ *
+ * @param {string} toCheck A string the start of which we want to check.
+ * @param {string} compareTo A string that should appear at the start of the toCheck.
+ * @returns {boolean} True if toCheck starts with compareTo, false otherwise.
+ */
+function startsWith(toCheck, compareTo) {
+    return toCheck.trim().toLowerCase().startsWith(compareTo.toLowerCase());
 }
 
 /**
- * Call this for SELECT and INSERT queries.
+ * Call this for SELECT queries.
  *
  * @param {string} sql The SQL query.
- * @param {function} treatResponse A callback function that is used to parse/label the 
- *                                  response that the query returns, if successful.
  * @returns {Promise<JSON>} JSON object that contains response data or error message, if the query was unsuccessful.
  */
 async function selectQuery(sql) {
     return await nonCriticalQuery(sql, "select", async (result) => {
-        let rows = [];
+        let rows_ = [];
         for (let key in result) {
-            rows.push(result[key]);
+            rows_.push(result[key]);
         }
-        return rows;
+        return {
+            query: "OK",
+            rows: rows_
+        }
     });
 }
 
+/**
+ * Call this for INSERT queries.
+ *
+ * @param {string} sql The SQL query.
+ * @returns {Promise<JSON>} JSON object that contains response data or error message, if the query was unsuccessful.
+ */
 async function insertQuery(sql) {
     return await nonCriticalQuery(sql, "insert", async (result) => {
         return await {
@@ -63,6 +85,15 @@ async function insertQuery(sql) {
     })
 }
 
+/**
+ * A helper function for insertQuery and selectQuery.
+ *
+ * @param {string} sql The SQL query.
+ * @param {string} type "select" or "insert"
+ * @param {function} treatResponse This function is called to format the query response, if 
+ *                                  the query was successful.
+ * @returns {Promise<JSON>} JSON object that contains response data or error message, if the query was unsuccessful.
+ */
 async function nonCriticalQuery(sql, type, treatResponse) {
     if (!startsWith(sql, type)) {
         await Promise.reject("Invalid use of " + type + "Query.");
@@ -73,19 +104,16 @@ async function nonCriticalQuery(sql, type, treatResponse) {
         result = await database.query(sql);
     }
     catch(err) {
+        const errResponse = getSQLErrorResponse(err);
         database.close();
-        return getErrResponse("Query failed.");
+        return errResponse;
     }
 
-    let treated = await treatResponse(result);
+    const treated = await treatResponse(result);
     database.close();
 
-    return await {
-        status: "OK",
-        response: treated
-    }
+    return await getSuccessfulResponse(treated);
 }
-
 
 /**
  * Generate a JSON error response.
@@ -101,45 +129,178 @@ function getErrResponse(cause) {
 }
 
 /**
- * Call this for UPDATE and DELETE queries.
+ * Formats the error response to give some information about the query error
+ * (type, code, error number, SQL message).
+ *
+ * @param {Error} err Error thrown by the database query function.
+ * @returns {JSON} Formatted error response.
+ */
+function getSQLErrorResponse(err) {
+    return getErrResponse({
+        type: "SQL Error",
+        code: err.code,
+        errno: err.errno,
+        sqlMessage: err.sqlMessage
+    });
+}
+
+/**
+ * Formats a successful response. Enables all responses to follow the same format.
+ *
+ * @param {JSON} response Preformatted query-specific response.
+ * @returns {JSON} A formatted response.
+ */
+function getSuccessfulResponse(response_) {
+    return {
+        status: "OK",
+        response: response_
+    }
+}
+
+/**
+ * Call this for DELETE queries.
  *
  * @param {string} sql The SQL query.
- * @param {function} treatResponse A callback function that is used to parse/label the 
- *                                  response that the query returns, if successful.
  * @returns {Promise<JSON>} JSON object that contains response data or error message, if the query was unsuccessful.
  */
-async function criticalQuery(sql, tableName, tableKey, token, treatResult) {
+async function deleteQuery(sql, entryTable, entryID) {
+    if (!startsWith(sql, "delete")) {
+        await Promise.reject("Invalid use of deleteQuery.");
+    }
+
+    const database = new Database(databaseConfig);
+
+    let tokenQuery = "SELECT * FROM TokenControl "
+    tokenQuery += "WHERE table_name = ? AND table_key = ?";
+    tokenQuery = mysql.format(tokenQuery, [entryTable, entryID]);
+
+    let queryResult = undefined;
+    try {
+        queryResult = await database.query(tokenQuery);
+    }
+    catch(err) {
+        const errResponse = getSQLErrorResponse(err);
+        database.close();
+        return errResponse;
+    }
+    
+    if (queryResult.length == 1) {
+        database.close();
+        return getErrResponse("Entry is being modified and cannot be deleted.");
+    }
+
+    // free to delete
 
     let result = undefined;
     try {
-        let query = "SELECT * FROM TokenControl "
-        query += "WHERE token = ? AND table_name = ? AND table_key = ?";
-
-        result = await database.query(query, [token, tableName, tableKey])
+        result = await database.query(sql);
     }
     catch(err) {
+        const errResponse = getSQLErrorResponse(err);
         database.close();
-        return getErrResponse("Query failed.");
+        return errResponse;
     }
 
-    if (result.length != 1) {
+    const response = {
+        query: "OK",
+        affectedRows: result.affectedRows
+    }
+    database.close();
+    return await getSuccessfulResponse(response);
+}
+
+
+/**
+ * Call this for DELETE queries.
+ *
+ * @param {string} sql The SQL query.
+ * @returns {Promise<JSON>} JSON object that contains response data or error message, if the query was unsuccessful.
+ */
+async function updateQuery(sql, entryTable, entryID, token) {
+    if (!startsWith(sql, "update")) {
+        await Promise.reject("Invalid use of updateQuery.");
+    }
+
+    const database = new Database(databaseConfig);
+
+    let tokenQuery = "SELECT * FROM TokenControl "
+    tokenQuery += "WHERE token = ? AND table_name = ? AND table_key = ?";
+    tokenQuery = mysql.format(tokenQuery, [token, entryTable, entryID]);
+
+    let queryResult = undefined;
+    try {
+        queryResult = await database.query(tokenQuery);
+    }
+    catch(err) {
+        const errResponse = getSQLErrorResponse(err);
+        database.close();
+        return errResponse;
+    }
+    
+    if (queryResult.length != 1) {
         database.close();
         return getErrResponse("Invalid token.");
     }
-    // else token is valid
 
+    // Token is valid. Execute query:
+
+    let result = undefined;
+    try {
+        result = await database.query(sql);
+    }
+    catch(err) {
+        const errResponse = getSQLErrorResponse(err);
+        database.close();
+        return errResponse;
+    }
+
+    const response = {
+        query: "OK",
+        affectedRows: result.affectedRows
+    };
+
+    // delete token
+    const deleteQuery = "DELETE FROM TokenControl WHERE token = ?";
+    let deleteResult = undefined;
+    try {
+        deleteResult = await database.query(deleteQuery, [token]);
+    }
+    catch(err) {
+        console.log("ERROR WHEN DELETING A TOKEN (" + token + "):");
+        console.log(JSON.stringify(getSQLErrorResponse(err)));
+    }
+    if (deleteResult.affectedRows != 1) {
+        console.log("ERROR WHEN DELETING A TOKEN (" + token + ")!");
+    }
+    
+
+    database.close();
+    return await getSuccessfulResponse(response);
 }
 
 
 
 
+
+
+
+
+
 selectQuery("SELECT * FROM Patient LIMIT 1").then((response) => {
-    console.log("Select was:\n" + JSON.stringify(response));
+    console.log("Select was:\n    " + JSON.stringify(response));
 });
 
-insertQuery("insert into Patient (patient_no, patient_name, patient_surname) values ('k1234101', 'Luka', 'Kralj')")
+
+insertQuery("insert into Patient (patient_no, patient_name, patient_surname) values ('lukakralj', 'Luka', 'Kralj')")
 .then((response) => {
-    console.log("Insert was:\n" + JSON.stringify(response));
+    console.log("Insert was:\n    " + JSON.stringify(response));
 });
 
+deleteQuery("DELETE FROM Patient WHERE patient_no = 'lukakralj'", "Patient", "lukakralj").then((response) => {
+    console.log("Delete was:\n    " + JSON.stringify(response));
+});
+
+updateQuery("UPDATE Patient SET patient_name = 'Luka - modified' WHERE patient_no = 'lukakralj'", "Patient", "lukakralj", "token").then((response) => {
+    console.log("Update was:\n    " + JSON.stringify(response));
+})
 
