97992f81e9a5750ff3b8560a78c819d44923806d
Danilo Del Busso <danilo.delbusso1@gmail.com>
2019-02-21 16:53:54 +0000

    Extend json-controller module

--- client/lib/json-controller.js
+++ client/lib/json-controller.js
@@ -2,33 +2,44 @@
  * Controller functions for json files reading and writing
  * @module json-controller
  * @author Danilo Del Busso
- * @version 0.0.1
+ * @version 0.0.2
  */
 
 const fs = require('fs');
+
 /**
     * Returns the content of the JSON file
     * @param {string} path The absolute path of the config file
-    * @returns {json} The config in JSON format
+    * @return {json} The config in JSON format, null if the file doesn't exist or is not a properly formatted JSON object
     * @example <caption>Example usage of getJSON.</caption>
     * getJSON(__dirname + '/config.json')
     */
 function getJSON(path) {
     if (fs.existsSync(path)) {
         const data = fs.readFileSync(path);
-        try {
-            const json = JSON.parse(data);
-            return json;
-        } catch (e) {
-            console.error(`The file at ${path} is not in JSON format`);
-            return null;
-        }
+        return isJSON(data)
     } else {
         console.error(`There is no JSON file at ${path}`)
         return null;
     }
 }
-module.exports = {
-    getJSON
+
+/**
+ * Check if the data is a properly formatted JSON file
+ * @param {string} data 
+ * @return {JSON} null if the data was not a properly formatted JSON object, the data in JSON if otherwise
+ */
+function isJSON(data) {
+    try {
+        const json = JSON.parse(data);
+        return json;
+    } catch (e) {
+        console.error(`The file at ${path} is not in JSON format`);
+        return null;
+    }
 }
 
+module.exports = {
+    getJSON,
+    isJSON
+}
