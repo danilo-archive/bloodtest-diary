068fe22d8d07b762aeffe291981ef86c255a9b4d
Jakub <jakub.cerven@gmail.com>
2019-02-22 15:50:04 +0000

    Fix unexpected type coercion

--- client/public/js/renderer.js
+++ client/public/js/renderer.js
@@ -2,7 +2,7 @@ const remote = require('electron').remote;
 
 (function handleWindowControls() {
     document.onreadystatechange = () => {
-        if (document.readyState == "complete") {
+        if (document.readyState === "complete") {
             init();
         }
     };
@@ -14,17 +14,14 @@ const remote = require('electron').remote;
         const closeButton = document.getElementById('close-button')
 
         minButton.addEventListener("click", event => {
-            window = remote.getCurrentWindow();
             window.minimize();
         });
 
         maxButton.addEventListener("click", event => {
-            window = remote.getCurrentWindow();
             window.maximize();
         });
 
         closeButton.addEventListener("click", event => {
-            window = remote.getCurrentWindow();
             window.close();
         });
     }
