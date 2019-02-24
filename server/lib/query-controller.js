15288c0e22c3a935be764342f3cba816be46fe64
mateusznowak98 <mateusz.michal.nowak98@gmail.com>
2019-02-24 22:34:53 +0000

    Separate SQL from app.js

--- server/lib/query-controller.js
+++ server/lib/query-controller.js
@@ -1,6 +1,107 @@
 const databaseController = require('./db_controller/db-controller.js');
-const tokenGenerator = require('./token-generator.js');
 
+/**
+* Get all the patients from the database
+* @return {JSON} result of the query
+**/
+async function getAllPatients()
+{
+  let sql = "Select * From Patient;";
+  return await selectQueryDatabase(sql)
+}
+
+/**
+*Get all the tests from the database
+* @return {JSON} result of the query
+**/
+async function getAllTests()
+{
+  let sql = "Select * From Test;";
+  return await selectQueryDatabase(sql)
+}
+
+/**
+* Get all the tests from a specific patient from the database
+* @param {String} patientId - id of a patient
+* @return {JSON} result of the query
+**/
+async function getTestsOfPatient(patientId){
+  let sql = `Select * From Test Where patient_no = ${patientId}`;
+  return await selectQueryDatabase(sql)
+}
+
+/**
+* Get all the tests on specific date from the database
+* @param {String} date - date (format: "YYYY-MM-DD")
+* @return {JSON} result of the query
+**/
+async function getAllTestsOnDate(date)
+{
+  let sql = `Select * From Test Where first_due_date = '${date}';`;
+  return await selectQueryDatabase(sql)
+}
+
+/**
+* Get all the overdue tests from the database
+* @return {JSON} result of the query
+**/
+async function getOverdueTests()
+{
+  let sql = `Select * From Test Join Patient On Patient.patient_no=Test.patient_no Where first_due_date < CURDATE() AND completed_status='no' `;
+  return await selectQueryDatabase(sql);
+}
+
+/**
+* Change the status of the test in the database
+* @param {String} testId - id of a test to change
+* @param {String} newStatus - new status of a test {enum: "completed"/"late"}
+* @return {JSON} result of the query
+**/
+async function changeTestStatus(testId, newStatus)
+{
+  //console.log("STATUS:" + newStatus);
+  var data = await databaseController.requestEditing("Test", testId).then( data => {return data;});
+  var token = data.response.token
+  //console.log(token);
+  if(token!=undefined)
+  {
+    switch(newStatus)
+    {
+      case "completed": {status = "yes"; date=`CURDATE()`; break;}
+      case "late": {status = "no"; date=`NULL`; break;}
+      default: return {success:false, response: data.response}
+    }
+    let sql = `UPDATE Test SET completed_status='${status}', completed_date=${date} WHERE test_id = ${testId};`;
+    //console.log(sql);
+    return {success:true , response: await databaseController.updateQuery(sql, "Test", testId, token).then(result => {return result.response})}
+  }
+  else {
+    return {success:false, response: data.response}
+  }
+}
+
+/**
+* Get all tests within the week from the database
+* @param {String} date - any date (from Monday to Friday) within the week to retrieve (format: "YYYY-MM-DD")
+* @return {JSON} result of the query
+**/
+async function getTestWithinWeek(date)
+{
+  var response = await Promise.all(getTestsDuringTheWeek(date))
+                              .then(days => {return checkMultipleQueriesStatus(days)})
+                              .then(data => {return data})
+  return response;
+}
+
+//=====================================
+//  HELPER FUNCTIONS BELOW:
+//=====================================
+
+/**
+* Produce multiple gueries on the database to retrieve test within the week
+* @param {String} date - date in the week to retrieve tests (format: "YYYY-MM-DD")
+* @return {Array} array of queries to run
+**/
 function getTestsDuringTheWeek(date)
 {
   var weekDay = new Date(date).getDay();
@@ -11,41 +112,15 @@ function getTestsDuringTheWeek(date)
     sql = `Select * From Test Join Patient on Test.patient_no=Patient.patient_no Where first_due_date = DATE_ADD('${date}', INTERVAL ${day} DAY);`;
     daysInWeek.push(databaseController.selectQuery(sql));
   }
-  //Get how many days to Sunday
-  //day = 7 -  weekDay;
-  //sql = `Select * From Test Join Patient on Test.patient_no=Patient.patient_no Where first_due_date = DATE_ADD('${date}', INTERVAL ${day} DAY);`;
-  //daysInWeek.push(databaseController.selectQuery(sql));
   return daysInWeek;
 }
 
-async function changeTestStatus(testId, newStatus)
-{
-  console.log("STATUS:" + newStatus);
-  var token = await databaseController.requestEditing("Test", testId).then( data => {
-    if(data.response.token){
-      return data.response.token
-    }
-    else{
-      return data;
-    }
-  });
-  console.log(token);
-  if(token!=undefined)
-  {
-    switch(newStatus)
-    {
-      case "completed": {status = "yes"; date=`CURDATE()`; break;}
-      case "late": {status = "no"; date=`NULL`; break;}
-    }
-    let sql = `UPDATE Test SET completed_status='${status}', completed_date=${date} WHERE test_id = ${testId};`;
-    console.log(sql);
-    return {success:true , response: await databaseController.updateQuery(sql, "Test", testId, token).then(data => {return data.response})}
-  }
-  else {
-    return {success:false, response: data.response}
-  }
-}
-
+/**
+* Run multiple gueries on the database
+* @param {Array} queries - array of queries to run
+* @return {Array} if no error: array of the results of the query
+* @return {JSON} if error: result of the faulty query
+**/
 function checkMultipleQueriesStatus(queries)
 {
   var data = [];
@@ -60,6 +135,11 @@ function checkMultipleQueriesStatus(queries)
   return {success:true, response:data};
 }
 
+/**
+* Run SELECT query on the database
+* @param {String} sql - SQL query
+* @return {JSON} result of the query
+**/
 async function selectQueryDatabase(sql)
 {
   var response = await databaseController.selectQuery(sql).then((queryResponse) =>{
@@ -74,14 +154,13 @@ async function selectQueryDatabase(sql)
   return response;
 }
 
-async function getTestWithinWeek(date)
-{
-  var response = await Promise.all(getTestsDuringTheWeek(date)).then(days => {return checkMultipleQueriesStatus(days)}).then(data => {return data})
-  return response;
-}
 
 module.exports = {
+    getAllPatients,
+    getAllTests,
+    getTestsOfPatient,
+    getAllTestsOnDate,
+    getOverdueTests,
     changeTestStatus,
-    selectQueryDatabase,
     getTestWithinWeek,
 };
