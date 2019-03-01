107588fc40fbd004181758c217ac1efe224f2a50
k1763585 <alessandro.amantini@kcl.ac.uk>
2019-03-01 13:23:56 +0000

    Fix bugs

--- client/src/components/calendarComponents/DayCell.js
+++ client/src/components/calendarComponents/DayCell.js
@@ -2,29 +2,20 @@ import React from 'react';
 import './DayCell.css';
 //import {render} from 'react-dom'
 
-class DayCell extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {selected: false};
-    this.day = this.props.dayOfMonth;
-    this.isFromThisMonth = this.props.isFromThisMonth;
-    this.selectDay = () => props.selectDay(this.day);
-  }
-  
-  render(){
-      let day = this.day;
-      let isFromThisMonth = this.isFromThisMonth;
+const DayCell = props => {
+      let day = props.dayOfMonth;
+      let isFromThisMonth = props.isFromThisMonth;
       return(
         <label style={{color: (!isFromThisMonth) ? '#0b989d' : 'white'}}>
-          <button className={'notSelected'} 
+          <button className={'notSelected'}
                   id={`${day}${isFromThisMonth}`}
-                  onClick={this.selectDay} >
+                  onClick={() => props.selectDay(day, isFromThisMonth)} >
             {day}
           </button>
         </label>
       );
   }
-}
+
 
 
 export default DayCell;
