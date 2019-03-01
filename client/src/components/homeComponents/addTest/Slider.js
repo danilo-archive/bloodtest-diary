c45705a6a79df80cdf94c387c238c7c410ce8ecb
AlvaroRausell <alvaro.rausellg@gmail.com>
2019-03-01 17:04:42 +0000

    Slider

---
+++ client/src/components/homeComponents/addTest/Slider.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import React from "react";
+
+const Slider = styled.input.attrs({ type: "range" })`
+    appearance:none;
+    outline:none;
+    width: 300px;
+    background-color: #e5e5e5;
+    height 5px;
+
+    &::-webkit-slider-thumb{
+        appearance:none;
+        width:15px;
+        height:15px;
+        background: #0d4e56;
+        transition: background 250ms;
+        border-radius:50%;
+
+    }
+    &:hover{
+
+        &::-webkit-slider-thumb{
+            background: #0b999d;
+        }
+    }
+`;
+
+export default props => {
+  return <Slider min={props.min} max={props.max} />;
+};
