e04fc68a8dd601e55cfa109fd8e9dba8a3d69ae6
JacopoMadaluni <jacopo.madaluni@gmail.com>
2019-03-05 16:32:23 +0000

    Add state config

--- client/src/components/homeComponents/editTest/EditTestView.js
+++ client/src/components/homeComponents/editTest/EditTestView.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import InfoBox from "./InfoBox";
 import TitleTab from "../addTest/TitleTab.js";
 import CalendarTable from "../../calendarComponents/Calendar";
+import {getServerConnect} from "../../../serverConnection.js";
 
 const DataContainer = styled.div`
   position: relative;
@@ -12,70 +13,106 @@ const DataContainer = styled.div`
 `;
 
 export default class EditTestView extends React.Component {
-  state = {
-    patient: { name: this.props.patient.name, id: this.props.patient.id },
-    test: {
-      id: this.props.test.id,
-      date: {
-        dueDate: this.props.test.date.dueDate,
-        frequency: this.props.test.date.frequency,
-        occurrences: this.props.test.date.occurrences
-      },
-      status: this.props.test.status
-    },
-    showCalendar: false
-  };
+
+
+
+  constructor(props){
+      super(props);
+      this.serverConnect = getServerConnect();
+      this.state = {
+        /*patient: { name: this.props.patient.name, id: this.props.patient.id },
+        test: {
+          id: this.props.test.id,
+          date: {
+            dueDate: this.props.test.date.dueDate,
+            frequency: this.props.test.date.frequency,
+            occurrences: this.props.test.date.occurrences
+          },
+          status: this.props.test.status
+        },
+        showCalendar: false*/
+        ready: false
+    };
+    this.init();
+
+  }
+
+  init(){
+      this.serverConnect.getTestInfo(this.props.testId, res => {
+         this.setState({
+             patient: {name: res.patient_name, id: res.patient_no},
+             test: {
+                 id: res.test_id,
+                 date: {
+                     dueDate: res.due_date,
+                     frequency: res.frequency,
+                     occurrences: res.occurrences
+                 },
+                 status: res.completed_status
+             },
+             showCalendar: false,
+             ready: true
+         });
+      });
+  }
+
+
   render() {
     console.log(this.state);
-    return (
-      <>
-        <div
-          style={{
-            width: "35rem",
-            height: "30rem",
-            background: "rgba(244, 244, 244,0.7)",
-            position: "relative"
-          }}
-        >
-          <TitleTab main={true}>Edit Appointment</TitleTab>
-          <div style={{ padding: "1rem 1rem" }}>
-            <InfoBox
-              label={"Full Name"}
-              text={this.state.patient.name}
-              icon="arrow-circle-right"
-              onClick={() => {
-                alert("This is supposed to open the patient view");
+    if (this.state.ready){
+        console.log("ready");
+        return (
+          <>
+            <div
+              style={{
+                width: "35rem",
+                height: "30rem",
+                background: "rgba(244, 244, 244,0.7)",
+                position: "relative"
               }}
-            />
-            {this.state.showCalendar ? (
-              <CalendarTable
-                style={{ width: "50%", top: "15%", left: "63%" }}
-                onDaySelected={day => {
-                  console.log(`Selected day ${day}`);
-                  this.setState({
-                    showCalendar: false,
-                    test: {
-                      ...this.state.test,
-                      date: {
-                        ...this.state.test.date,
-                        dueDate: day
-                      }
-                    }
-                  });
-                }}
-              />
-            ) : (
-              ``
-            )}
-            <InfoBox
-              label={"Due"}
-              text={this.state.test.date.dueDate}
-              icon="edit"
-              onClick={() => this.setState({ showCalendar: true })}
-            />
-          </div>
-        </div>
-      </>
-    );
+            >
+              <TitleTab main={true}>Edit Appointment</TitleTab>
+              <div style={{ padding: "1rem 1rem" }}>
+                <InfoBox
+                  label={"Full Name"}
+                  text={this.state.patient.name}
+                  icon="arrow-circle-right"
+                  onClick={() => {
+                    alert("This is supposed to open the patient view");
+                  }}
+                />
+                {this.state.showCalendar ? (
+                  <CalendarTable
+                    style={{ width: "50%", top: "15%", left: "63%" }}
+                    onDaySelected={day => {
+                      console.log(`Selected day ${day}`);
+                      this.setState({
+                        showCalendar: false,
+                        test: {
+                          ...this.state.test,
+                          date: {
+                            ...this.state.test.date,
+                            dueDate: day
+                          }
+                        }
+                      });
+                    }}
+                  />
+                ) : (
+                  ``
+                )}
+                <InfoBox
+                  label={"Due"}
+                  text={this.state.test.date.dueDate}
+                  icon="edit"
+                  onClick={() => this.setState({ showCalendar: true })}
+                />
+              </div>
+            </div>
+          </>
+        );
+    }else{
+        return ("");
+    }
   }
 }
