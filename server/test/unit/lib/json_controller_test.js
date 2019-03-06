baac158731feb549884de489b91f90fd70e59256
Danilo Del Busso <danilo.delbusso1@gmail.com>
2019-03-06 12:52:15 +0000

    Add json-controller-test.js to server app

---
+++ server/test/unit/lib/json_controller_test.js
@@ -0,0 +1,51 @@
+const chai = require('chai');
+const should = chai.should();
+const mock_fs = require('mock-fs');
+
+const json_controller = require('../../../lib/json-controller');
+
+
+afterEach(() => mock_fs.restore());
+
+/**
+ * Test if the getJSON returns the content of a file
+ */
+describe("Read JSON files", () => {
+    it('should return a json object', () => {
+        //create valid json file
+        mock_fs({
+            'test': {
+                'test.json': `{
+                        "port" : 4200,
+                        "staticFolder" : "/public",
+                        "indexFile" : "index.html"
+                    }`
+            }
+        });
+
+        const obj = json_controller.getJSON('test/test.json');
+        const expected_result = {
+            "port": 4200,
+            "staticFolder": "/public",
+            "indexFile": "index.html"
+        }
+        obj.should.deep.equal(expected_result);
+    });
+
+    it('should not find the file and return null', () => {
+        const obj = json_controller.getJSON('test/other_test.json');
+        should.not.exist(obj);
+    });
+
+    it('should return null because file is not json', () => {
+        //create file which does not use json format
+        mock_fs({
+            'test': {
+                'test_not_json.json': `this is not a JSON file`
+            }
+        });
+
+        const obj = json_controller.getJSON('test/test_not_json.json');
+        should.not.exist(obj);
+    })
+});
