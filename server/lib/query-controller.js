39157c8635241041789e10b0be49d31201b3be49
JacopoMadaluni <jacopo.madaluni@gmail.com>
2019-03-08 17:26:08 +0000

    Fix server conflict

--- server/lib/query-controller.js
+++ server/lib/query-controller.js
@@ -4,11 +4,6 @@ const calendarController = require("./calendar-controller.js");
 const _ = require("lodash");
 const logger = require('./action-logger');
 const dateformat = require('dateformat');
-<<<<<<< HEAD
-=======
-
->>>>>>> a22480e8c514c586828ad6782835cacc6c18417a
-
 
 /**
  * Get the patient given its patient number
