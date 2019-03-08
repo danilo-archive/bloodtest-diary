3ea24dc280d2342cc0605da8e4a981c6f2b5202a
Jakub Cerven <jakub.cerven@gmail.com>
2019-03-08 15:58:59 +0000

    Fix import statements

--- client/src/index.js
+++ client/src/index.js
@@ -3,18 +3,22 @@ import ReactDOM from "react-dom";
 import Login from "./components/login.js";
 import Home from "./components/home.js";
 import Header from "./components/header.js";
-import Patients from "./components/patients.js";
+import Patients from "./components/Patients.js";
+import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
 import { Route, Link, BrowserRouter as Router, Switch } from "react-router-dom";
 
 const routing = (
+
   <Router>
-    <div className={"routes"}>
-      <Route path="/" component={Header} />
-      <Route exact path="/" component={Login} />
+      <div className={"routes"}>
+          <Route path="/" component={Header} />
+
+
+          <Route exact path="/" component={Login} />
       <Route path="/home" component={Home} />
-      <Route path={"/patients"} component={Patients} />
+      <Route path={"/Patients"} component={Patients} />
     </div>
   </Router>
 );
