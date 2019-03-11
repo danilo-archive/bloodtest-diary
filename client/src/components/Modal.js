f5d951c6a580c6684a0674977599fbf23f777561
AlvaroRausell <alvaro.rausellg@gmail.com>
2019-03-11 18:09:02 +0000

    Modal Component

---
+++ client/src/components/Modal.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import Modal from "react-responsive-modal";
+
+export default styled(Modal)`
+  & > div {
+    padding: 0;
+    border: solid 2px black;
+  }
+`;
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import styled from "styled-components";
-import Modal from 'react-responsive-modal';
+import Modal from "./Modal";
 
 import Navbar from "./homeComponents/navbar";
 import OverduePatients from "./homeComponents/overduePatients";
@@ -11,200 +11,207 @@ import VerticalLine from "./homeComponents/calendarComponents/VerticalLine";
 import LoadingAnimation from "./loadingScreen/loadingAnimation";
 
 import EditTest from "./homeComponents/editTest/EditTestView";
-import {getNextDates, getMondayOfWeek, getCurrentWeek, getPreviousWeek, getNextWeek} from "../lib/calendar-controller";
-import {getServerConnect} from "../serverConnection.js";
-import {group, getNumberOfTestsInGroup} from "../lib/overdue-controller.js";
-import HTML5Backend from 'react-dnd-html5-backend'
-import { DragDropContext } from 'react-dnd'
+import {
+  getNextDates,
+  getMondayOfWeek,
+  getCurrentWeek,
+  getPreviousWeek,
+  getNextWeek
+} from "../lib/calendar-controller";
+import { getServerConnect } from "../serverConnection.js";
+import { group, getNumberOfTestsInGroup } from "../lib/overdue-controller.js";
+import HTML5Backend from "react-dnd-html5-backend";
+import { DragDropContext } from "react-dnd";
 import CustomDragLayer from "./homeComponents/CustomDragLayer.js";
-import './home.css';
+import "./home.css";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.serverConnect = getServerConnect();
+    this.serverConnect.joinMainPage();
 
-    constructor(props) {
-      super(props);
-      this.serverConnect = getServerConnect();
-      this.serverConnect.joinMainPage();
+    this.state = {
+      dashboardReady: false,
+      overdueReady: false,
+      weekDays: getCurrentWeek(),
+      overdueTests: {},
+      ongoingTests: {},
+      calendar: {},
+      openAddTestModal: false,
+      openEditTestModal: false,
+      editTestId: undefined,
+      editToken: undefined
+    };
+  }
 
+  onPatientsClick(event) {
+    this.props.history.push("patients");
+  }
 
-      this.state = {
-        dashboardReady: false,
-        overdueReady: false,
-        weekDays: getCurrentWeek(),
-        overdueTests: {},
-        ongoingTests: {},
-        calendar: {},
-        openAddTestModal: false,
-        openEditTestModal: false,
-        editTestId: undefined,
-        editToken: undefined
-      };
+  componentDidMount = () => {
+    this.initOverduePanel();
+    this.updateDashboard();
+    this.initCallbacks();
 
-    }
+    this.handleNext = this.handleNext.bind(this);
+    this.handlePrevious = this.handlePrevious.bind(this);
+    this.onPatientsClick = this.onPatientsClick.bind(this);
 
-    onPatientsClick(event) {
-        this.props.history.push("patients")
-    }
+    this.onAddTestOpenModal = this.onAddTestOpenModal.bind(this);
+    this.onAddTestCloseModal = this.onAddTestCloseModal.bind(this);
+  };
 
-    componentDidMount = () => {
-        this.initOverduePanel();
-        this.updateDashboard();
-        this.initCallbacks();
+  initCallbacks() {
+    this.initOnTestAdded();
+    this.initOnTestStatusChange();
+  }
 
-        this.handleNext = this.handleNext.bind(this);
-        this.handlePrevious = this.handlePrevious.bind(this);
-        this.onPatientsClick = this.onPatientsClick.bind(this);
+  initOnTestAdded() {
+    this.serverConnect.setOnTestAdded(newTest => {
+      this.updateDashboard();
+    });
+  }
 
-        this.onAddTestOpenModal = this.onAddTestOpenModal.bind(this);
-        this.onAddTestCloseModal = this.onAddTestCloseModal.bind(this);
-    }
-
-
-    initCallbacks() {
-      this.initOnTestAdded();
-      this.initOnTestStatusChange();
-    }
-
-    initOnTestAdded() {
-      this.serverConnect.setOnTestAdded(newTest => {
-          this.updateDashboard();
+  initOnTestStatusChange() {
+    this.serverConnect.setOnTestStatusChange((id, status) => {
+      this.modifyTest(id, test => {
+        test.completed_status = status;
+        return test;
       });
-    }
+    });
+  }
 
-    initOnTestStatusChange() {
-      this.serverConnect.setOnTestStatusChange((id, status) => {
-        this.modifyTest(id, test => {
-            test.completed_status = status;
-            return test;
-        });
+  initOnTestEdit() {
+    this.serverConnect.setOnTestEdit((id, newTest) => {
+      this.modifyTest(id, test => {
+        test = newTest;
+        return newTest;
       });
-    }
+    });
+  }
 
-    initOnTestEdit(){
-        this.serverConnect.setOnTestEdit((id, newTest) => {
-            this.modifyTest(id, test => {
-                test = newTest;
-                return newTest;
-            });
-        });
-    }
-
-    initOverduePanel() {
-      this.serverConnect.getOverdueTests(res => {
-        this.setState({
-            overdueTests: res,
-            overdueReady: true
-        });
+  initOverduePanel() {
+    this.serverConnect.getOverdueTests(res => {
+      this.setState({
+        overdueTests: res,
+        overdueReady: true
       });
-    }
+    });
+  }
 
-    updateDashboard(newWeek=undefined) {
-      let monday = newWeek ? newWeek[0] : this.state.weekDays[0];
-      newWeek = newWeek ? newWeek : this.state.weekDays;
-      this.serverConnect.getTestsInWeek(monday, res => {
-        this.setState({
-              ongoingTests: res[5],
-              calendar: res.slice(0, 5),
-              dashboardReady: true,
-              weekDays: newWeek
-          });
-        });
-    }
+  updateDashboard(newWeek = undefined) {
+    let monday = newWeek ? newWeek[0] : this.state.weekDays[0];
+    newWeek = newWeek ? newWeek : this.state.weekDays;
+    this.serverConnect.getTestsInWeek(monday, res => {
+      this.setState({
+        ongoingTests: res[5],
+        calendar: res.slice(0, 5),
+        dashboardReady: true,
+        weekDays: newWeek
+      });
+    });
+  }
 
-    modifyTest(id, modificationFunction){
-        for (var i = 0; i < this.state.overdueTests.length; ++i) {
-          let group = this.state.overdueTests[i];
-          for (var j = 0; j < group.tests.length ; ++j){
-              var test = group.tests[j];
-              if (test.test_id === id) {
-                let newOverdueTests = [...this.state.overdueTests];
-                let testToModify = newOverdueTests[i].tests[j]
-                let modifiedTest = modificationFunction(testToModify);
-                newOverdueTests[i].tests[j] = modifiedTest;
-                this.setState({overdueTests: newOverdueTests});
-              }
-          }
-        }
-        // check if it's ongoing
-        for (var i = 0; i < this.state.ongoingTests.length; ++i) {
-          var test = this.state.ongoingTests[i];
-          if (test.test_id === id) {
-            let newOngoingTests = [...this.state.ongoingTests];
-            let testToModify = newOngoingTests[i];
-            let modifiedTest = modificationFunction(testToModify);
-            newOngoingTests[i] = modifiedTest;
-            this.setState({ongoingTests: newOngoingTests});
-            return;
-          }
-        }
-        //check if it's in the current calendar
-        for (var i = 0; i < this.state.calendar.length; ++i) {
-          var day = this.state.calendar[i];
-          for (var j = 0; j < day.length; ++j) {
-            var test = day[j];
-            if (test.test_id === id) {
-              let newCalendar = [...this.state.calendar];
-              let testToModify = newCalendar[i][j];
-              let modifiedTest = modificationFunction(testToModify);
-              newCalendar[i][j] = modifiedTest;
-              this.setState({calendar: newCalendar});
-              return;
-            }
-          }
+  modifyTest(id, modificationFunction) {
+    for (var i = 0; i < this.state.overdueTests.length; ++i) {
+      let group = this.state.overdueTests[i];
+      for (var j = 0; j < group.tests.length; ++j) {
+        var test = group.tests[j];
+        if (test.test_id === id) {
+          let newOverdueTests = [...this.state.overdueTests];
+          let testToModify = newOverdueTests[i].tests[j];
+          let modifiedTest = modificationFunction(testToModify);
+          newOverdueTests[i].tests[j] = modifiedTest;
+          this.setState({ overdueTests: newOverdueTests });
         }
       }
-
-    handleNext(event) {
-      let nextWeek = getNextWeek([...this.state.weekDays]);
-      this.updateDashboard(nextWeek);
     }
-
-    handlePrevious(event) {
-      let previousWeek = getPreviousWeek([...this.state.weekDays]);
-      this.updateDashboard(previousWeek);
+    // check if it's ongoing
+    for (var i = 0; i < this.state.ongoingTests.length; ++i) {
+      var test = this.state.ongoingTests[i];
+      if (test.test_id === id) {
+        let newOngoingTests = [...this.state.ongoingTests];
+        let testToModify = newOngoingTests[i];
+        let modifiedTest = modificationFunction(testToModify);
+        newOngoingTests[i] = modifiedTest;
+        this.setState({ ongoingTests: newOngoingTests });
+        return;
+      }
     }
+    //check if it's in the current calendar
+    for (var i = 0; i < this.state.calendar.length; ++i) {
+      var day = this.state.calendar[i];
+      for (var j = 0; j < day.length; ++j) {
+        var test = day[j];
+        if (test.test_id === id) {
+          let newCalendar = [...this.state.calendar];
+          let testToModify = newCalendar[i][j];
+          let modifiedTest = modificationFunction(testToModify);
+          newCalendar[i][j] = modifiedTest;
+          this.setState({ calendar: newCalendar });
+          return;
+        }
+      }
+    }
+  }
 
-    onAddTestOpenModal = selectedDate => {
-      this.setState({ openAddTestModal: true, selectedDate });
-    };
+  handleNext(event) {
+    let nextWeek = getNextWeek([...this.state.weekDays]);
+    this.updateDashboard(nextWeek);
+  }
 
-    onAddTestCloseModal = () => {
-      this.setState({ openAddTestModal: false, selectedDate: undefined });
-    };
+  handlePrevious(event) {
+    let previousWeek = getPreviousWeek([...this.state.weekDays]);
+    this.updateDashboard(previousWeek);
+  }
 
-    onEditTestOpenModal = testId => {
-        this.serverConnect.requestTestEditing(testId, token => {
-          if (token != undefined){
-            this.setState({openEditTestModal: true, editTestId: testId, editToken: token});
-          }
+  onAddTestOpenModal = selectedDate => {
+    this.setState({ openAddTestModal: true, selectedDate });
+  };
+
+  onAddTestCloseModal = () => {
+    this.setState({ openAddTestModal: false, selectedDate: undefined });
+  };
+
+  onEditTestOpenModal = testId => {
+    this.serverConnect.requestTestEditing(testId, token => {
+      if (token != undefined) {
+        this.setState({
+          openEditTestModal: true,
+          editTestId: testId,
+          editToken: token
         });
-    };
+      }
+    });
+  };
 
-    onEditTestCloseModal = () => {
-        // TODO remove token if not used
-        this.setState({openEditTestModal: false, editTestId: undefined, editToken: undefined});
-    };
+  onEditTestCloseModal = () => {
+    // TODO remove token if not used
+    this.setState({
+      openEditTestModal: false,
+      editTestId: undefined,
+      editToken: undefined
+    });
+  };
 
-    // ----------------- ADDED TEST METHODS --------------------------
+  // ----------------- ADDED TEST METHODS --------------------------
 
-    moveTest(from, to, test){
+  moveTest(from, to, test) {}
 
-    }
-
-    handleDrop = (section, testId) => {
-
-    }
-// 
-    render() {
-      if (this.state.dashboardReady && this.state.overdueReady) {
-        return (
-
+  handleDrop = (section, testId) => {};
+  //
+  render() {
+    if (this.state.dashboardReady && this.state.overdueReady) {
+      return (
         <div className={"home"}>
           <div className={"dashboard"}>
             <CustomDragLayer snapToGrid={true} />
             <div className={"overduePatients"}>
               <OverduePatients
-                notificationNumber={getNumberOfTestsInGroup(this.state.overdueTests)}
+                notificationNumber={getNumberOfTestsInGroup(
+                  this.state.overdueTests
+                )}
                 anytimeAppointments={this.state.overdueTests}
                 editTest={this.onEditTestOpenModal}
               />
@@ -212,9 +219,9 @@ class Home extends Component {
             <div className={"rightSideDash"}>
               <div className={"navbar"}>
                 <Navbar
-                    onPrev={this.handlePrevious}
-                    onNext={this.handleNext}
-                    onPatientsClick={this.onPatientsClick}
+                  onPrev={this.handlePrevious}
+                  onNext={this.handleNext}
+                  onPatientsClick={this.onPatientsClick}
                 />
               </div>
               <div className={"bottomSideDash"}>
@@ -226,7 +233,7 @@ class Home extends Component {
                     editTest={this.onEditTestOpenModal}
                   />
                 </div>
-                <div className={"divider"}></div>
+                <div className={"divider"} />
                 <div className={"ongoingWeekly"}>
                   <OngoingWeekly
                     currentMonday={this.currentMonday}
@@ -251,39 +258,41 @@ class Home extends Component {
               />
             </Modal>
             <Modal
-                open={this.state.openEditTestModal}
-                onClose={this.onEditTestCloseModal}
-                showCloseIcon={false}
-                style={modalStyles}
-                center
+              open={this.state.openEditTestModal}
+              onClose={this.onEditTestCloseModal}
+              showCloseIcon={false}
+              style={modalStyles}
+              center
             >
               <EditTest
-                 testId = {this.state.editTestId}
-                 closeModal={this.onEditTestCloseModal}
-                 token={this.state.editToken}
+                testId={this.state.editTestId}
+                closeModal={this.onEditTestCloseModal}
+                token={this.state.editToken}
               />
             </Modal>
           </div>
-         </div>
-        );
-      } else {
-        return (
-          <div style={{
+        </div>
+      );
+    } else {
+      return (
+        <div
+          style={{
             position: "absolute",
             left: "50%",
             top: "50%",
-            transform: "translate(-50%,-50%)",
-          }}>
-          <LoadingAnimation/>
+            transform: "translate(-50%,-50%)"
+          }}
+        >
+          <LoadingAnimation />
         </div>
-      )
-      }
+      );
     }
   }
+}
 
-  const modalStyles = {
-    padding: 0
-  };
+const modalStyles = {
+  padding: 0
+};
 
-  export default DragDropContext(HTML5Backend)(Home);
+export default DragDropContext(HTML5Backend)(Home);
 //export default Home;
