8251fcc8fb9aedb4c65fd02369e4ce23247c91d6
JacopoMadaluni <jacopo.madaluni@gmail.com>
2019-03-11 11:42:18 +0000

    Fix typo error

--- client/src/components/loadingScreen/LoadingScreen.js
+++ client/src/components/loadingScreen/LoadingScreen.js
@@ -10,7 +10,7 @@ export default class extends Component{
                 <LoadingAnimation/>
                 <div className={"dialog"}>
                   <h3 className="override">Connection to server lost...</h3>
-                  <p className="override">If the problem presists contact your IT department.</p>
+                  <p className="override">If the problem persists contact your IT department.</p>
                 </div>
               </div>
         )
