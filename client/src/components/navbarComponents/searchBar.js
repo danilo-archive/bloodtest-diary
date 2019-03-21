d20ad1dc9c68beec6a484ccf9e1a3044fb022b38
k1763585 <alessandro.amantini@kcl.ac.uk>
2019-03-21 20:23:15 +0000

    Calendar appears normally

--- client/src/components/navbarComponents/searchBar.js
+++ client/src/components/navbarComponents/searchBar.js
@@ -6,44 +6,42 @@ import calendarIcon from "../../images/calendar.png";
 
 const Container = styled.div`
 
-vertical-align: middle;
 font-size: 1.05rem;
 height: 11px;
 width: 200px;
 display: flex;
-align-items: center;
 margin-left: 10px;
 font-family: "Rajdhani", sans-serif;
 color: black;
-text-align: center;
+align-items: bottom;
 
 .calendar-icon {
-border: none;
-height: 40px;
-color: #1b1b1b;
-font-size: 15px;
-outline: none;
-margin-left: 20px;
-margin-right: 10px;
-margin-bottom: 3px;
+  border: none;
+  height: 40px;
+  color: #1b1b1b;
+  font-size: 15px;
+  outline: none;
+  margin-left: 20px;
+  margin-right: 10px;
+  margin-top: -17px;
 }
 
 .calendar-icon:not(:placeholder-shown) + .label {
-font-size: 0px;
-top: 5px;
-color: v;
+  font-size: 0px;
+  top: 5px;
+  color: v;
 }
 
 .calendar-icon:focus ~ .label {
-font-size: 0px;
-top: 5px;
-color:  #97a9a9;
-transition: all 0.5s ease;
+  font-size: 0px;
+  top: 5px;
+  color:  #97a9a9;
+  transition: all 0.5s ease;
 }
 
 .calendar-icon:focus ~ .highlight {
-width: calc(100% - 50px);
-transition: all 1s ease;
+  width: calc(100% - 50px);
+  transition: all 1s ease;
 }
 `;
 
@@ -53,12 +51,6 @@ export default class SearchBar extends Component {
         this.state = {
           showCalender: false,
         };
-        this.closeCalendar = () => {
-          this.setState = {
-            showCalender: true,
-          };
-          console.log(this.state.showCalender)
-        }
     }
 
     render(){
@@ -67,12 +59,12 @@ export default class SearchBar extends Component {
               <img className={"calendar-icon"}
                    alt={"Calendar"} 
                    src={calendarIcon}
-                   onClick= {() => {this.setState({ showCalendar: !this.state.showCalendar })}}
+                   onClick= {() => this.setState({ showCalendar: !this.state.showCalendar })}
               />
               Date picker
 
         {this.state.showCalendar ? (
-          <CalendarTable closeCalendar={this.closeCalendar}/>
+          <CalendarTable />
         ) : (
           <></>
         )}
