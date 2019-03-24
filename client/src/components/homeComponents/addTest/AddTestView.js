a3b3843d02674df7c1fc547653fa924d19fa6a86
Jakub <jakub.cerven@gmail.com>
2019-03-24 20:58:41 +0000

    Add horizontal line to add test

--- client/src/components/homeComponents/addTest/AddTestView.js
+++ client/src/components/homeComponents/addTest/AddTestView.js
@@ -16,6 +16,7 @@ const DataContainer = styled.div`
 
 const ButtonContainer = styled.div`
   width: 100%;
+  background: white;
   display: flex;
   flex-wrap: wrap;
   align-content: flex-start;
@@ -60,6 +61,10 @@ const AddButton = styled.button`
     color: white;
   }
   outline: none;
+`;
+
+const Hr = styled.hr`
+  width: 99%;
 `;
 
 export default class AddTestView extends React.Component {
@@ -265,11 +270,8 @@ export default class AddTestView extends React.Component {
                 })
               }
             />
-            <ButtonContainer
-              style={{
-                padding: "4px 0 4px 0"
-              }}
-            >
+            <Hr/>
+            <ButtonContainer>
               <CloseButton onClick={this.props.closeModal}>Close</CloseButton>
               <AddButton onClick={this.onDoneClick}>Add test</AddButton>
             </ButtonContainer>
